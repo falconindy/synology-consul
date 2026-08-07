6bf3a84d6ca3f9fe8add9b8da039f14b4929951e
Dave Reisner <d@falconindy.com>
2026-08-07 10:46:48 -0400

    bump version to 2.0.2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export CONSUL_VERSION="${CONSUL_VERSION:-1.22.6}"
+export CONSUL_VERSION="${CONSUL_VERSION:-2.0.2}"
 export PACKAGE_VERSION="${CONSUL_VERSION}-1000"
 export OS="${OS:-linux}"
 export ARCH="${ARCH:-amd64}"
